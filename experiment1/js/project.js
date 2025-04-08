7bc1524e319e7784594c08c2d49b097553790d62
Lunar Fox <lunarfoxbox@gmail.com>
2025-04-07 21:41:42 -0700

    Updated generator

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -24,24 +24,17 @@ class MyProjectClass {
 
 function main() {
   const fillers = {
-    adventurer: ["My dude", "Bro", "WesBot", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "勇者", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
-    pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
-    post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
-    people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca", "帥氣"],
-    item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
-    num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
-    looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
-    loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
-    baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
-    message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
-    
+    nation: ["Revaron", "KiKi", "Inkabara", "Foxia", "California", "Orgeon", "Washington"],
+    describe: ["Peoples", "Free", "Great", "Capitist", "Socalist", "National", "$describe", "Vassel"],
+    connection: ["of ", ""],
+    government: ["Republic", "Kingdom", "Empire", "Commonwealth", "Holy $government", "United $describe", ""],
+    message: ["declares war", "allied", "conquired", "annexed", "invaded", "held off", "is trading with", "rebulit", "is rebuilding ", "signed a ceasfire with", "are at peace with"],
+    continueMessage: ["meanwhile $formNation $message $formNation", "in opposition of $formNation", "with the aid of $formNation", "shocking all"],
+    formNation: ['$describe $government $connection $nation']
   };
   
-  const template = `$adventurer, heed my $message!
-  
-  I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
-  
-  It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
+  const template = `World News!
+  $formNation $message $formNation $continueMessage
   `;
   
   
